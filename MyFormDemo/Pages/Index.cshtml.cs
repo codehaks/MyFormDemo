c6c5cf887bc317732abe5f4b91653eda9149ff24
Hakim Ghods <ghods.hakim@hotmail.com>
2021-11-01 11:22:04 +0330

    Add project files.

---
+++ MyFormDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFormDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public string Color { get; set; }
+
+        public IActionResult OnPost()
+        {
+            TempData["color"]= Color;
+            return RedirectToPage("./result");
+        }
+    }
+}
